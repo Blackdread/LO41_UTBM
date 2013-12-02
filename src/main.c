487e924695c7c84f461c961a77f4de907d911097
Blackdread <jojo.julien@gmail.com>
2013-12-02 16:58:02 +0100

    1er commit test

---
+++ src/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  LO41
+//
+//  Created by yoann on 02/12/13.
+//  Copyright (c) 2013 yoann. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
